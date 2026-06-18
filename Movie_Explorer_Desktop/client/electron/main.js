c7c97036cd85d01f336077a28ae8495abf50018e
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-06-18 18:43:47 +0530

    feat : create electrone main process

---
+++ Movie_Explorer_Desktop/client/electron/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+  });
+
+  win.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
